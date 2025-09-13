4f73d59062c8202711199ae947791fd4a5d13dfb
lolkile <167018345+Mifa4@users.noreply.github.com>
2025-09-13 19:37:50 +0300

    Calc
    Only calculator :)

--- Core/VisualCore/VCore/VCore/main.cpp
+++ Core/VisualCore/VCore/VCore/main.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 int main() {
 	system("chcp 1251");
-	cout << "Hello world!\n";
+	int a,c;
+	char d;
+
+	cout << "Number 1:\n";
+	cin >> a;
+	cout << "What operation need to do:\n";
+	cin >> d;
+	cout << "Number 2:\n";
+	cin >> c;
+	if (d == '+') {
+		cout << a + c;
+	}
+	else if (d == '-') {
+		cout << a - c;
+	}
+	else if (d == '*') {
+		cout << a * c;
+	}
+	else if (d == ':') {
+		cout << a / c;
+	}
+	else if (d == '^') {
+		cout << (a + c) / 2;
+	}
 	return 0;
+}
+
+namespace VisualMethods {
+
 }
