878d58ef8ec835d7534ae57f33cec4731e2465c1
ingener2010 <167018345+Mifa4@users.noreply.github.com>
2025-09-20 19:49:29 +0400

    Math problem
    Problem with math method

--- Core/MathCore/MCore/MCore/main.cpp
+++ Core/MathCore/MCore/MCore/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+int* ArrInv(int numberArr[],int size) {
+	for (int num = 0;num < size;num++)
+		numberArr[num] = -numberArr[num];
+	return numberArr;
+}
+
 int main() {
+	int a[] = { 1,2,3,4,5,6,7,8,9 };
+	int* res = ArrInv(a, 9);
 	return 0;
 }
