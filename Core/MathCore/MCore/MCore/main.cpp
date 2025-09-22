53e3905568d911963fafa4fc927c9a7f29fe2116
Mifsi24 <167018345+Mifa4@users.noreply.github.com>
2025-09-22 20:43:21 +0400

    Touch and sort
    Added touch checking methods, and sort by bubble.
    Need: solve problems from old versions,
    add more sorts.

--- Core/MathCore/MCore/MCore/main.cpp
+++ Core/MathCore/MCore/MCore/main.cpp
@@ -2,26 +2,27 @@
 
 using namespace std;
 
+//Array Invertor
 int* ArrInv(int numberArr[], int size) {
 	for (int num = 0;num < size;num++)
 		numberArr[num] = -numberArr[num];
 	return numberArr;
 }
-
+//Array Sum
 int ArrSum(int numberArr[], int size) {
 	int res = 0;
 	for (int num = 0; num < size; num++)
 		res += numberArr[num];
 	return res;
 }
-
+//Array multiplication
 int ArrMul(int numberArr[], int size) {
 	int res = 0;
 	for (int num = 0; num < size; num++)
 		res *= numberArr[num];
 	return res;
 }
-
+//Array Division
 int ArrD(int numberArr[], int size) {
 	int res;
 	if (size > 1)
@@ -30,31 +31,31 @@ int ArrD(int numberArr[], int size) {
 		res = res /= numberArr[num + 1];
 	return res;
 }
-
+//Degree
 int Deg(int a, int b) {
 	int res;
 	if (b == 0)
-		return 0;
+		return 1;
 	else
 		res = a * a;
-	for (int i = 1; i <= b; i++) {
+	for (int i = 2; i <= b; i++)
 		res = res * a;
-	}
 	return res;
 }
-
-int DDR(int res, int to) {
+//Division Degree to 10 for Fraction
+int DDF(int res, int to) {
 	int i = 1;
-	while (i <= 100) {
-		if (Deg(10,i) % res == 0) {
-			return (10 * i);
+	while (i <= 9) {
+		int Prom = Deg(10, i);
+		if (Prom % res == 0) {
+			return Prom;
 		}
 		i++;
 	}
 	return -2401201;
 }
-
-int* DWR(int num1, int num2) {
+//Division with Fraction
+int* DWF(int num1, int num2) {
 	int _int = num1 / num2;
 	int res[] = { _int,0,0 };
 	if (num1 % num2 == 0) {
@@ -62,7 +63,7 @@ int* DWR(int num1, int num2) {
 	}
 	if (num2 % 2 == 0 or num2 % 5 == 0) {
 		if (num2 % 3 != 0 and num2 % 7 != 0 and num2 % 9 != 0) {
-			int i = DDR(num1, num2);
+			int i = DDF(num1, num2);
 			res[1] = num1;
 			res[2] = i;
 			return res;
@@ -77,10 +78,74 @@ int* DWR(int num1, int num2) {
 		return res;
 	}
 }
+//Touch Smaller check
+bool TS(int number, int checkNumber) {
+	if (checkNumber <= number)
+		return true;
+	return false;
+}
+//Touch Biggerer check
+bool TB(int number, int checkNumber) {
+	if (checkNumber >= number)
+		return true;
+	return false;
+}
+//Touch Equalas check
+bool TE(int number, int checkNumber) {
+	if (checkNumber == number)
+		return true;
+	return false;
+}
+//Object Touch Check
+bool OTC(int borders[2], int checkBorders[2]) {
+	if (borders[0] > borders[1])
+		borders[0], borders[1] = borders[1], borders[0];
+	if (checkBorders[0] > checkBorders[1])
+		checkBorders[0], checkBorders[1] = checkBorders[1], checkBorders[0];
+	if (TB(borders[0], checkBorders[1]) and TS(borders[0], checkBorders[0]))
+		return true;
+	if (TS(borders[1], checkBorders[0]) and TB(borders[1], checkBorders[1]))
+		return true;
+	return false;
+}
+//Arrray Sort Bubble From Small
+int* ASBS(int arrToSort[], int size) {
+	bool swaped;
+	for (int i = 0; i < size; i++) {
+		swaped = false;
+		for (int b = 0; b < size - i; b++) {
+			if (arrToSort[b] > arrToSort[b + 1]) {
+				arrToSort[b], arrToSort[b + 1] = arrToSort[b + 1], arrToSort[b];
+				swaped = true;
+			}
+		}
+		if (!swaped)
+			break;
+	}
+}
+
+//Arrray Sort Bubble From Big
+int* ASBB(int arrToSort[], int size) {
+	bool swaped;
+	for (int i = 0; i < size; i++) {
+		swaped = false;
+		for (int b = 0; b < size - i; b++) {
+			if (arrToSort[b] < arrToSort[b + 1]) {
+				swaped = true;
+				arrToSort[b], arrToSort[b + 1] = arrToSort[b + 1], arrToSort[b];
+			}
+		}
+		if (!swaped)
+			break;
+	}
+	return arrToSort;
+}
 
 int main() {
-	int* _int = DWR(5, 5);
-	for(int b = 0; b < 3; b++)
+	int* _int = DWF(10, 3);
+	for (int b = 0; b < 3; b++) {
 		cout << _int[b];
+		cout << "\n";
+	}
 	return 0;
 }
